9bbffdd928b9ad98652659640c0f101e838c18c0
Cyryl Gotkowicz <cyrylgotkowicz@gmail.com>
2023-05-23 13:17:40 +0100

    objects challenge

--- cohort.js
+++ cohort.js
@@ -5,7 +5,7 @@ const cohort = {
 };
 
 const print = (object) => {
-    console.log(`${object.id} - ${object.name} - ${object.names.length} students in this cohort`);
+    return `${object.id} - ${object.name} - ${object.names.length} students in this cohort`;
 };
 
-module.exports = print;
+console.log(print(cohort))
