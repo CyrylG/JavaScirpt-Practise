33b2c8a58b52a5dfe9d3dbfcd9ccc862d2a34bf0
Cyryl Gotkowicz <cyrylgotkowicz@gmail.com>
2023-05-23 15:08:58 +0100

    generate message map operation

--- generateMessages.js
+++ generateMessages.js
@@ -1,8 +1,14 @@
-const generateMessages = (names) => {
-    const messages = names.map(name => `Hi ${name}!`);
-    console.log(messages)
+const namesAndDiscounts = [
+    { name: 'Anna', discount: 50 },
+    { name: 'Laura', discount: 40 },
+    { name: 'Josh', discount: 30 },
+    { name: 'Min', discount: 50 },
+    { name: 'Karla', discount: 60 }
+  ];
+
+const generateMessages = (namesAndDiscounts) => {
+    const message = namesAndDiscounts.map(person => `Hi ${person.name}! ${person.discount}% off our best candies for you today!`);
+    console.log(message);
 }
 
-const names = ['Anna', 'Laura', 'Josh', 'Min', 'Karla']
-
-generateMessages(names)
+generateMessages(namesAndDiscounts)
