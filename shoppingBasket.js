4312c733bc3dd6f3c922d574f506e0f32306d3aa
Cyryl Gotkowicz <cyrylgotkowicz@gmail.com>
2023-05-24 20:21:15 +0100

    Classes challenge

---
+++ shoppingBasket.js
@@ -0,0 +1,20 @@
+class ShoppingBasket {
+    constructor() {
+        this.items = [];
+    }
+
+    getTotalPrice() {
+        let total = 0;
+        this.items.forEach((item) => {
+            total += item.price;
+        })
+
+        return total;
+    }
+
+    addItem(item) {
+        this.items.push(item);
+    }
+}
+
+module.exports = ShoppingBasket;
